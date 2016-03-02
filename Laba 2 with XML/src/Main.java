1e905ddb93c70c050df4ed72cd884481d8443265
LittleBelka <kompp@yandex.ru>
2016-03-02 03:44:40 +0300

    Laba 2 updates

--- Laba 2 with XML/src/Main.java
+++ Laba 2 with XML/src/Main.java
@@ -1,28 +1,23 @@
+import javax.xml.transform.TransformerException;
 import java.io.File;
 import java.io.IOException;
 
-
 public class Main {
 
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException, TransformerException, IOException {
 
-        try{
-            ConfigurationRead xPathCaller = new ConfigurationRead("configuration.xml");
+        ConfigurationRead xPathCaller = new ConfigurationRead("configuration.xml");
 
-            File directory1 = new File(xPathCaller.getProperty("locationFolderOne"));
-            File directory2 = new File(xPathCaller.getProperty("locationFolderTwo"));
-            ScanDirectory scDir = new ScanDirectory(directory1, directory2, "Временный_файл.tmp", true);
-            scDir.begin();
+        File directory1 = new File(xPathCaller.getProperty("locationFolderOne"));
+        File directory2 = new File(xPathCaller.getProperty("locationFolderTwo"));
+        ScanDirectory scDir = new ScanDirectory(directory1, directory2, "Временный_файл.tmp", true);
+        scDir.begin();
 
-            /*ObjectInputStream fileStream = new ObjectInputStream(new FileInputStream("Временный_файл.tmp"));
-            HashMap<String, FileMetadata> tmpM = (HashMap<String, FileMetadata>) fileStream.readObject();
-            for(Map.Entry<String, FileMetadata> fm : tmpM.entrySet()) {
-                System.out.println(fm.getKey());
-            }*/
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        /*ObjectInputStream fileStream = new ObjectInputStream(new FileInputStream("Временный_файл.tmp"));
+        HashMap<String, FileMetadata> tmpM = (HashMap<String, FileMetadata>) fileStream.readObject();
+        for(Map.Entry<String, FileMetadata> fm : tmpM.entrySet()) {
+            System.out.println(fm.getKey());
+        }*/
     }
 
 }
